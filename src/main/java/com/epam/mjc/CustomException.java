53a9c1db33521ec210ee70f6da2387ec1e3e3a18
Leclassique <75534147+Leclassique@users.noreply.github.com>
2023-10-12 16:11:12 +0300

    Completed task

---
+++ src/main/java/com/epam/mjc/CustomException.java
@@ -0,0 +1,6 @@
+package com.epam.mjc;
+
+public class CustomException extends IllegalArgumentException{
+    public CustomException(String message){
+    super(message);
+}}
--- src/main/java/com/epam/mjc/StudentManager.java
+++ src/main/java/com/epam/mjc/StudentManager.java
@@ -1,21 +1,27 @@
 package com.epam.mjc;
 
 
+import java.util.NoSuchElementException;
+
 public class StudentManager {
 
   private static final long[] IDs = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
 
   public Student find(long studentID) {
-    return Student.getValueOf(studentID);
-  }
-
-  public static void main(String[] args) {
-    StudentManager manager = new StudentManager();
-
-    for (int i = 0; i < IDs.length; i++) {
-      Student student = manager.find(IDs[i]);
-      System.out.println("Student name " + student.getName());
+        try{
+            return Student.getValueOf(studentID);
+        }catch (NoSuchElementException e){
+            throw new CustomException("Could not find student with ID " + studentID);
+        }
     }
 
-  }
+    public static void main(String[] args) {
+        StudentManager manager = new StudentManager();
+
+        for (int i = 0; i < IDs.length; i++) {
+            Student student = manager.find(IDs[i]);
+            System.out.println("Student name " + student.getName());
+        }
+
+    }
 }
